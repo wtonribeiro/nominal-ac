b1adfa606ec723b396b795bfdf4351c453f19703
wtonribeiro <wtonribeiro@gmail.com>
2018-09-22 08:59:14 -0300

    Updating the build.sh file

--- Impl-Equiv/build.sh
+++ Impl-Equiv/build.sh
@@ -1,4 +1,13 @@
-ocamlc -c Basic.ml
-ocamlopt -c Basic.ml
-ocamlc -c Equiv.ml
-ocamlopt -c Equiv.ml
+ocamlc -c Original_Generated_Equiv.ml
+ocamlopt -c Original_Generated_Equiv.ml
+ocamlc -c Adjusted_Generated_Equiv.ml
+ocamlopt -c Adjusted_Generated_Equiv.ml
+ocamlc -c Basics.ml
+ocamlopt -c Basics.ml
+ocamlc -c Improved_Equiv.ml
+ocamlopt -c Improved_Equiv.ml
+ocamlfind ocamlc -linkpkg -thread -package ocamlgraph Perfect_Matching.ml
+ocamlfind ocamlopt -linkpkg -thread -package ocamlgraph Perfect_Matching.ml
+ocamlc -c Graph_Equiv.ml
+ocamlopt -c Graph_Equiv.ml
+
