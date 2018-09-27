50ab534b90be45f16c9572d6dfc3143efe49e899
Washington L. de Carvalho Segundo <wtonribeiro@gmail.com>
2018-09-27 07:44:37 -0300

    Update results.r

--- Impl-Equiv/Experiments/results.r
+++ Impl-Equiv/Experiments/results.r
@@ -1,14 +1,12 @@
  # ============================================================================
  # Project     : Nominal A, AC and C Unification
- # File        : Basics.ml
+ # File        : results.r
  # Authors     : Washington Lu\'is R. de Carvalho Segundo and
  #               Mauricio Ayala Rinc\'on 
  #               Universidade de Bras\'ilia (UnB) - Brazil
  #               Group of Theory of Computation
  
- # Description : This file contains the definitions of f_terms, flattening,
- #               the freshness checking function, iterator function
- #               and the difference set operator.
+ # Description : 
  # Last Modified On: Sep 27, 2018.
  # ============================================================================
 
